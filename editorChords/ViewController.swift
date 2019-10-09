1c45292c286be8066b76f62608921d3abcee4035
svinogr <sv>
2019-10-09 11:34:49 +0300

    Initial Commit

---
+++ editorChords/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  editorChords
+//
+//  Created by Up Devel on 09/10/2019.
+//  Copyright © 2019 Up Devel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
