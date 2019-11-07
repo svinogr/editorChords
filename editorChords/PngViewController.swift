7c7e76f50d45d88ec951e1df7bc0bfb994a8252c
s <v>
2019-11-07 11:26:21 +0300

    reface

--- editorChords/PngViewController.swift
+++ editorChords/PngViewController.swift
@@ -12,7 +12,7 @@ class PngViewController: UIViewController {
     private let im1 = UIImage(named: "1")!
     private let im2 = UIImage(named: "2")!
     private let size = CGSize(width: 240, height: 240)
-
+    
     @IBOutlet weak var png: UIImageView!
     
     var lads: [Lad]!
@@ -20,11 +20,11 @@ class PngViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-      for i in lads {
-                    i.printStr()
-                   // i.printStrWithNumberStr()
-                }
-createPNG()
+        for i in lads {
+            i.printStr()
+            // i.printStrWithNumberStr()
+        }
+        createPNG()
         // Do any additional setup after loading the view.
     }
     
@@ -33,7 +33,8 @@ createPNG()
         
         createGriff()
         createAckord()
-
+        createLadImage()
+        
         let  newImage:UIImage = UIGraphicsGetImageFromCurrentImageContext()!
         UIGraphicsEndImageContext()
         
@@ -45,6 +46,7 @@ createPNG()
         for i in 1..<lads.count {
             var image: UIImage
             
+            
             for j in 0..<lads[i].strings.count {
                 if lads[i].strings[j] == false {
                     image = im1
@@ -52,12 +54,11 @@ createPNG()
                     image = im2
                 }
                 
-                let x = j * 24
+                let x = j * 24 + 24
                 let y = i * 24
                 
                 let areaSize =  CGRect(x: x, y: y, width: 24, height: 24)
                 image.draw(in: areaSize)
-                
             }
         }
     }
@@ -73,14 +74,37 @@ createPNG()
                 image = im2
             }
             
-            let x = j * 24
+            let x = j * 24 + 24
             let y =  0
             
             let areaSize =  CGRect(x: x, y: y, width: 24, height: 24)
             image.draw(in: areaSize)
-            }
-
+        }
     }
     
-
+    private func createLadImage() {
+        let numberOfStartLad = lads[1].id
+        var stepForImagePosition = 1
+        let textColor = UIColor.red
+        let textFont = UIFont(name: "Helvetica Bold", size: 24)!
+        
+        let textFontAttributes = [
+            NSAttributedString.Key.font: textFont,
+            NSAttributedString.Key.foregroundColor: textColor,
+        ]
+        
+        for i in numberOfStartLad..<numberOfStartLad + 5 {
+            UIGraphicsBeginImageContext(CGSize(width: 24, height: 24))
+            
+            let letter = String(i)
+            
+            let xLetter = 0
+            let yLetter = stepForImagePosition * 24
+            let areaSizeLad = CGRect(x: xLetter, y: yLetter, width: 24, height: 24)
+            
+            UIGraphicsEndImageContext()
+            letter.draw(in: areaSizeLad, withAttributes: textFontAttributes)
+            stepForImagePosition = stepForImagePosition + 1
+        }
+    }
 }
